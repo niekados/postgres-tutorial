ceeb22ec9d761a348713305863073935dd52ee3f
niekados <tiny.post@yahoo.com>
2024-05-21 16:18:26 +0000

    Add query 6 to sql-orm.py

--- sql-orm.py
+++ sql-orm.py
@@ -72,3 +72,18 @@ base.metadata.create_all(db)
 # for album in albums:
 #     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
 
+# Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.MediaTypeId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | "
+        )
